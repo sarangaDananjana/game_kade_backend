a4edd76faa6ff19629f8b49b9d1fc69d436706bf
sarangaDananjana <dananjanasaranga@gmail.com>
2026-05-09 16:46:17 +0530

    small update

--- food_delivery/settings.py
+++ food_delivery/settings.py
@@ -17,8 +17,9 @@ import dj_database_url
 from dotenv import load_dotenv
 
 
-# Load environment variables from .env file
+# Load environment variables from .env file and .env.prod
 load_dotenv()
+load_dotenv('.env.prod')
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
