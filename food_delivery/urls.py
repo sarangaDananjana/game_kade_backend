9c4ffd0ccaaf1adb560695ee1a407a81af3f9a87
sarangaDananjana <dananjanasaranga@gmail.com>
2026-06-06 13:33:04 +0530

    updated

--- food_delivery/urls.py
+++ food_delivery/urls.py
@@ -36,7 +36,10 @@ urlpatterns = [
     path('shop/', include('multivender.urls')),
 ]
 
-# Serve media files (like product images) during development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+# Serve media files (like product images) through Django directly
+from django.urls import re_path
+from django.views.static import serve
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
