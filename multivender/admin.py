ce09701c83725164138ffad1abf648007bb27b8a
sarangaDananjana <dananjanasaranga@gmail.com>
2026-07-18 01:48:27 +0530

    updated

--- multivender/admin.py
+++ multivender/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import DeliveryZone, Vendor, Product, OrderLocation, Order, OrderItem
+from .models import DeliveryZone, Vendor, Product, OrderLocation, Order, OrderItem, DeliveryDistanceTier
 
 class ProductInline(admin.TabularInline):
     model = Product
@@ -7,9 +7,9 @@ class ProductInline(admin.TabularInline):
 
 @admin.register(Vendor)
 class VendorAdmin(admin.ModelAdmin):
-    list_display = ('shop_name', 'owner', 'is_pickup_only', 'is_active', 'created_at', 'logo', 'cover_image')
+    list_display = ('shop_name', 'owner', 'is_pickup_only', 'is_active', 'created_at', 'h3_index')
     list_filter = ('is_active', 'is_pickup_only')
-    search_fields = ('shop_name', 'owner__phone_number')
+    search_fields = ('shop_name', 'owner__phone_number', 'h3_index')
     inlines = [ProductInline]
 
 class OrderItemInline(admin.TabularInline):
@@ -18,7 +18,7 @@ class OrderItemInline(admin.TabularInline):
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('id', 'vendor', 'user', 'status', 'total_amount', 'created_at')
+    list_display = ('id', 'vendor', 'user', 'status', 'total_amount', 'delivery_fee', 'created_at')
     list_filter = ('status', 'created_at')
     search_fields = ('id', 'user__phone_number', 'vendor__shop_name', 'delivery_code')
     inlines = [OrderItemInline]
@@ -31,8 +31,13 @@ class ProductAdmin(admin.ModelAdmin):
 
 @admin.register(OrderLocation)
 class OrderLocationAdmin(admin.ModelAdmin):
-    list_display = ('name', 'user', 'unique_identity', 'is_system_defined')
+    list_display = ('name', 'user', 'unique_identity', 'is_system_defined', 'h3_index')
     list_filter = ('is_system_defined',)
-    search_fields = ('name', 'unique_identity', 'user__phone_number')
+    search_fields = ('name', 'unique_identity', 'user__phone_number', 'h3_index')
+
+@admin.register(DeliveryDistanceTier)
+class DeliveryDistanceTierAdmin(admin.ModelAdmin):
+    list_display = ('grid_distance', 'delivery_fee')
+    search_fields = ('grid_distance',)
 
 admin.site.register(DeliveryZone)
